fac1f160f06d9c50aa0261d5b068ead90d6b34e9
Aryan Jha <aryanjha@gmail.com>
2020-04-04 19:20:14 -0400

    added user/password file

---
+++ Main.py
@@ -0,0 +1,84 @@
+import os, json
+
+# instantiate file with a empty list if file is empty
+file_size = os.path.getsize("UserPass.json")
+if file_size <= 1:
+    with open("UserPass.json", mode='w', encoding='utf-8') as file:
+        json.dump([], file)
+
+
+def check_existing_user(name):
+    """loads json file which stores username and password combinations
+    and compares the current username to previously saved usernames"""
+    with open("UserPass.json") as f:
+        list_of_user = json.load(f)
+        for users in list_of_user:
+            cname = users.pop('username')
+            if cname == name:
+                return True
+    return False
+
+
+def check_password(u_name, p_word):
+    """loads the json file and compares the entered username and password
+    combination with the saved ones"""
+    with open('UserPass.json', 'r') as f:
+        list_of_users = json.load(f)
+        for users in list_of_users:
+            cname = users.pop('username')
+            if cname == u_name:
+                cpass = users.pop('password')
+                if cpass == p_word:
+                    return True
+                else:
+                    return False
+
+
+def write_user(u_name, p_word):
+    """opens the json file and stores the values into a list of usernames and passwords,
+    then appends the new user's information to the list and dumps the updated list back
+    into the file"""
+    with open('UserPass.json', 'r') as f:
+        new_user = {'username': u_name, 'password': p_word}
+        list_of_users = json.load(f)
+        list_of_users.append(new_user)
+    with open('UserPass.json', 'w') as f:
+        json.dump(list_of_users, f)
+
+
+choice = str(input("Would you like to login or create an account? (Login/Create)\n")).lower()
+if choice == "create":
+    username = str(input("Please enter a user name: "))
+    while check_existing_user(username):
+        print("That username already exists, please try another")
+        username = str(input("Please enter a user name: "))
+
+    # username doesnt exist, continue with creating a new user
+    password = str(input("Please enter a password: "))
+    temp_password = str(input("Please confirm your password: "))
+    while password != temp_password:
+        print("Passwords did not match please try again")
+        temp_password = str(input("Please confirm your password: "))
+
+    if password == temp_password:
+        print("Creating new user....")
+        write_user(username, password)
+
+elif choice == "login":
+    username = str(input("Please enter your username: "))
+    while not(check_existing_user(username)):
+        print("Sorry, your username was not found")
+        username = str(input("Please enter your username again: "))
+
+    password = str(input("Please enter your password: "))
+    counter = 0
+    while not(check_password(username, password)):
+        counter += 1
+        if counter >= 3:
+            print("Invalid amount of tries. Exiting...\n")
+            os._exit(1)
+        print("Sorry, your password is incorrect\nYou have " + str(3-counter) + " tries left")
+        password = str(input("Please enter your password again: "))
+
+    # successful login performed
+    print("\nWelcome " + username + "!")
